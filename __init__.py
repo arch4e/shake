d7543db9aea1aaf83e2549ad8dc77c3c456db031
arch4e <contact@arch4e.com>
2023-11-16 22:37:15 +0900

    update: change add-on version

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ bl_info = {
     'name'    : 'ShaKe',
     'category': '3D View',
     'location': '',
-    'version' : (1,2,0),
+    'version' : (2,0,0),
     'blender' : (3,0,0),
     'author'  : 'arch4e'
 }
