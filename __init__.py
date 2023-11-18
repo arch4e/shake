8f7efa32b92ea2da80570bd42ca25cbf9c0edd62
arch4e <contact@arch4e.com>
2023-11-18 22:17:29 +0900

    feat: add a panel to create shape keys from csv file

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@
 import bpy
 
 from .operator.context_menu_extention import ShaKe_OT_align_by_prefix, ShaKe_OT_move_shape_key_below_selected
+from .operator.create_shape_keys_from_csv import ShaKe_OT_create_shape_keys_from_csv
 from .operator.order_management import (
     ShaKe_OT_add_prefix,
     ShaKe_OT_move_prefix_down,
@@ -12,9 +13,11 @@ from .operator.order_management import (
 )
 from .operator.selector import ShaKe_OT_select_shape_key
 from .operator.transcribe import ShaKe_OT_select_destination, ShaKe_OT_select_shape_keys, ShaKe_OT_transcribe_shape_keys
+from .property.create_shape_keys_from_csv import ShaKe_PG_create_shape_keys_from_csv
 from .property.order_management import ShaKe_PG_order_mgmt_prefix
 from .property.transcribe import ShaKe_PG_transcribe
 from .ui.context_menu_extention import ShaKe_MT_move_shape_key_below_selected, ShaKe_MT_shape_keys_extended_menu
+from .ui.create_shape_keys_from_csv import ShaKe_PT_create_shape_keys_from_csv
 from .ui.order_management import ShaKe_PT_order_management, UI_UL_ShaKe_order_management
 from .ui.selector import ShaKe_PT_shape_keys_selector
 from .ui.transcribe import ShaKe_PT_transcribe
@@ -37,6 +40,7 @@ classes = [
     ShaKe_MT_move_shape_key_below_selected,
     ShaKe_OT_add_prefix,
     ShaKe_OT_align_by_prefix,
+    ShaKe_OT_create_shape_keys_from_csv,
     ShaKe_OT_move_prefix_down,
     ShaKe_OT_move_prefix_up,
     ShaKe_OT_move_shape_key_below_selected,
@@ -47,8 +51,10 @@ classes = [
     ShaKe_OT_select_shape_keys,
     ShaKe_OT_sync_selected_obj_prefix_list,
     ShaKe_OT_transcribe_shape_keys,
+    ShaKe_PG_create_shape_keys_from_csv,
     ShaKe_PG_order_mgmt_prefix,
     ShaKe_PG_transcribe,
+    ShaKe_PT_create_shape_keys_from_csv,
     ShaKe_PT_order_management,
     ShaKe_PT_shape_keys_selector,
     ShaKe_PT_transcribe,
