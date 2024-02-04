c0497ef1df4bf87d2cb636d063b56fc1fc3a2943
arch4e <contact@arch4e.com>
2024-02-04 15:14:13 +0900

    change: remove the transcribe feature

--- __init__.py
+++ __init__.py
@@ -11,16 +11,11 @@ from .operator.order_management import (
     ShaKe_OT_remove_prefix,
     ShaKe_OT_sync_selected_obj_prefix_list
 )
-from .operator.selector import ShaKe_OT_select_shape_key
-from .operator.transcribe import ShaKe_OT_select_destination, ShaKe_OT_select_shape_keys, ShaKe_OT_transcribe_shape_keys
 from .property.create_shape_keys_from_csv import ShaKe_PG_create_shape_keys_from_csv
 from .property.order_management import ShaKe_PG_order_mgmt_prefix
-from .property.transcribe import ShaKe_PG_transcribe
 from .ui.context_menu_extention import ShaKe_MT_move_shape_key_below_selected, ShaKe_MT_shape_keys_extended_menu
 from .ui.create_shape_keys_from_csv import ShaKe_PT_create_shape_keys_from_csv
 from .ui.order_management import ShaKe_PT_order_management, UI_UL_ShaKe_order_management
-from .ui.selector import ShaKe_PT_shape_keys_selector
-from .ui.transcribe import ShaKe_PT_transcribe
 from .util.props_register import register as props_register, unregister as props_unregister
 
 
@@ -46,18 +41,11 @@ classes = [
     ShaKe_OT_move_shape_key_below_selected,
     ShaKe_OT_rearrange_by_prefix_list,
     ShaKe_OT_remove_prefix,
-    ShaKe_OT_select_destination,
-    ShaKe_OT_select_shape_key,
-    ShaKe_OT_select_shape_keys,
     ShaKe_OT_sync_selected_obj_prefix_list,
-    ShaKe_OT_transcribe_shape_keys,
     ShaKe_PG_create_shape_keys_from_csv,
     ShaKe_PG_order_mgmt_prefix,
-    ShaKe_PG_transcribe,
     ShaKe_PT_create_shape_keys_from_csv,
     ShaKe_PT_order_management,
-    ShaKe_PT_shape_keys_selector,
-    ShaKe_PT_transcribe,
     UI_UL_ShaKe_order_management,
 ]
 
